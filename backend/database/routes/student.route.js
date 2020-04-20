e21aa2a538372abeb4c1ec95f83501b5f5096d66
Estelle OLAWINSKI <estelle@fr.mpitech.com>
2020-04-20 12:32:23 +0200

    routes

--- backend/database/routes/student.route.js
+++ backend/database/routes/student.route.js
@@ -26,3 +26,49 @@ studentRoute.route("/").get((req, res) => {
     }
   });
 });
+
+//Get single student
+studentRoute.route("/read-student/:id").get((req, res) => {
+  Student.findById(req.params.id),
+    (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.json(data);
+      }
+    };
+});
+
+//Update student
+studentRoute.route("/update-student/:id").put((req, res, next) => {
+  Student.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error);
+      } else {
+        res.json(data);
+        console.log("Student successfully updated");
+      }
+    }
+  );
+});
+
+//Delete student
+studentRoute.route("/delete-student/:id").delete((req, res, next) => {
+  Student.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
+
+module.exports = studentRoute;
